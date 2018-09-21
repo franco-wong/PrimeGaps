62fb03c511520b2a71c794dc73337c2a35084d14
StephPitman <32081012+StephPitman@users.noreply.github.com>
2018-09-21 14:25:33 -0400

    First Coding Session

--- src/prime_gaps.c
+++ src/prime_gaps.c
@@ -10,8 +10,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	char input_string[] = "1000";
+	mpz_t n;
+	mpz_t p;
+	mpz_init(n);
+	mpz_init(p);
+	mpz_set_ui(n,50);
+	mpz_set_ui(p,0);
+	int flag;
+	flag = mpz_set_str(n,input_string, 10);
+	mpz_out_str(stdout,10,n);
+	printf("\n");
+	mpz_nextprime(p, n);
+	mpz_out_str(stdout,10,p);
+	printf("\n");
 }
