fb4933df3d5abd1d2df78fafa39a756cb333e82f
StephPitman <32081012+StephPitman@users.noreply.github.com>
2018-09-22 14:21:14 -0400

    First Version of Dividing Work
    All typed up, just need to do a little debugging after.

--- src/prime_gaps.c
+++ src/prime_gaps.c
@@ -27,4 +27,42 @@ int main(void) {
 	mpz_nextprime(p, n);
 	mpz_out_str(stdout,10,p);
 	printf("\n");
+
+	//dividing up the work
+	mpz_t np;
+	mpz_init(np);
+	//int P = 8;
+	mpz_t P;
+	mpz_init(P);
+	mpz_init_set(P, 8);
+
+	mpz_fdiv_q(np,n,P);
+
+	mpz_t proc_work[P];
+	int x;
+	for(x=0;x<P;x++){
+		if(x < mpz_mod(n,P)){
+			mpz_add(proc_work[x],np,1);
+		}
+		else{
+			mpz_init_set(proc_work[x],np);
+		}
+	}
+
+	mpz_t first_part;
+
+	mpz_t proc_start[P];
+	for(x=0;x<P;x++){
+		mpz_mul(first_part,np,x);
+		if(mpz_cmp(p,mpz_mod(n,P))>0){
+			mpz_add(proc_start[x],first_part,P);
+		}
+		else{
+			mpz_add(proc_start[x],first_part,n);
+		}
+
+	}
+
+
+
 }
